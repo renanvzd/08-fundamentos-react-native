199a9ef9b89cf705eab7b9b21e48561ed824a6fc
Renan Veronez Drechsler <renanvz@outlook.com>
2021-01-20 18:40:47 -0300

    Load products

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -35,9 +35,10 @@ const Dashboard: React.FC = () => {
 
   useEffect(() => {
     async function loadProducts(): Promise<void> {
-      // TODO
-    }
+      const response = await api.get('/products');
 
+      setProducts(response.data);
+    }
     loadProducts();
   }, []);
 
